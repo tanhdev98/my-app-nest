1bbcf0dcf3584ebe5367d2e58359854f0fd6c39c
tanhdev98 <tanhdev98@gmail.com>
2024-08-19 15:05:58 +0700

    create base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { UsersModule } from '@/modules/users/users.module';
+import { ReviewsModule } from '@/modules/reviews/reviews.module';
+import { RestaurantsModule } from '@/modules/restaurants/restaurants.module';
+import { OrdersModule } from '@/modules/orders/orders.module';
+import { OrderDetailModule } from '@/modules/order.detail/order.detail.module';
+import { MenuItemsModule } from '@/modules/menu.items/menu.items.module';
+import { MenusModule } from '@/modules/menus/menus.module';
+import { MenuItemOptionsModule } from '@/modules/menu.item.options/menu.item.options.module';
+import { LikesModule } from '@/modules/likes/likes.module';
+
 @Module({
   imports: [
+    ReviewsModule,
+    RestaurantsModule,
+    OrdersModule,
+    OrderDetailModule,
+    MenuItemsModule,
+    MenusModule,
+    MenuItemOptionsModule,
+    LikesModule,
     UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
