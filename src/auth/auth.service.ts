21830cbe856a0c3926bb26309e613510af6aca5f
tanhdev98 <tanhdev98@gmail.com>
2024-08-20 10:55:59 +0700

    integration passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,17 +10,19 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
     const isValidPassword = await comparePasswordHelper(pass, user.password);
 
-    if (!isValidPassword) {
-      throw new UnauthorizedException();
-    }
+    if (!user || !isValidPassword) return null;
 
-    const payload = { sub: user._id, username: user.email };
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: this.jwtService.sign(payload),
     };
   }
 }
