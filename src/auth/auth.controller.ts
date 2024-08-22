2d03ba270a837b0ba567f8141d32f923c13e777d
tanhdev98 <tanhdev98@gmail.com>
2024-08-22 10:17:59 +0700

    config send mail

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,10 +14,14 @@ import { LocalAuthGuard } from '@/auth/passport/local-auth.guard';
 import { JwtAuthGuard } from '@/auth/passport/jwt-auth.guard';
 import { Public } from '@/decorator/customize';
 import { CreateAuthDto } from '@/auth/dto/create-auth.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -35,5 +39,17 @@ export class AuthController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService.sendMail({
+      to: 'tanhtest98@gmail.com',
+      subject: 'Testing Nest MailerModule ✔',
+      text: 'welcome',
+      html: '<b>hello world</b>',
+    });
+    return 'ok';
   }
 }
